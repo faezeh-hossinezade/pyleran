0491ce440002963d8c145d0df9f93f5b9fd3752f
faeze hosseinazde <fazyhossinezade@gmail.com>
2024-04-05 16:49:50 +0330

    third commit

--- Assignment15/test.py
+++
@@ -1,9 +0,0 @@
-import random
- 
-# using random() to generate a random number
-# between 0 and 1
-print("A random number between 0 and 1 is : ", end="")
-print(random.random())
- 
-# # using seed() to seed a random number
-print(random.seed()) 
